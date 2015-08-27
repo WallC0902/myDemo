0ed934334d173af74cc1a3c7a4ac379a717782f1
WallC0902 <623124582@qq.com>
2015-08-27 15:32:04 +0800

    增加branch的打印信息。

--- branch.java
+++ branch.java
@@ -1,5 +1,5 @@
 public void class branch(){
 	public branch(){
-		System.out.println("this is a branch!");
+		System.out.println("this is a branch,demo branch was destroy , now you are a java file");
 	}
 }
