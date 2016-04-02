1d9fbe1be11368df1e675e4ac7a08767f0e95f61
Wufeng Wang <623124582@qq.com>
2016-04-02 23:46:54 +0800

    Update branch.java

--- branch.java
+++ branch.java
@@ -1,5 +1,6 @@
 public void class branch(){
 	public branch(){
+		//git pull = git fetch + git merge  在pull的过程中merge可能会出错
 		System.out.println("this is a branch,demo branch was destroy , now you are a java file");
 	}
 }
